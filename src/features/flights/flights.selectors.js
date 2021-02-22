2c4503f6b72ea8e41baf2058e8c6ec0124e203f6
Alex Klyde <alexeykalayda@gmail.com>
2021-02-22 12:40:24 +0200

    refactored

--- src/features/flights/flights.selectors.js
+++ src/features/flights/flights.selectors.js
@@ -1,7 +1,7 @@
 import { createSelector } from 'reselect';
 import { format } from 'date-fns';
 import { today } from './flights.utils';
- 
+
 export const flightsListSelector = state => state.flights.flightsList;
 
 export const filteredFlightsListSelector = createSelector([flightsListSelector], flightsList => {
