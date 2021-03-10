308b6a2defe9cb9ce6d9fc66b2c486ac7c25b53a
Alex Klyde <alexeykalayda@gmail.com>
2021-03-10 14:57:39 +0200

    refactored

--- src/features/flights/flights.selectors.js
+++ src/features/flights/flights.selectors.js
@@ -5,12 +5,8 @@ import { todayDate } from './flights.utils';
 export const isFetchingSelector = state => state.flights.isFetching;
 export const flightsListSelector = state => state.flights.flightsList;
 
-export const sortedListSelector = createSelector([flightsListSelector], flightsList => {
-  return flightsList.sort((a, b) => new Date(a.localTime) - new Date(b.localTime));
-});
-
-export const filteredFlightsListSelector = createSelector([sortedListSelector], flightsList => {
-  return flightsList.filter(
-    flight => format(new Date(flight.localTime), 'dd-MM-yyyy') === todayDate,
-  );
+export const filteredFlightsListSelector = createSelector([flightsListSelector], flightsList => {
+  return flightsList
+    .filter(flight => format(new Date(flight.localTime), 'dd-MM-yyyy') === todayDate)
+    .sort((a, b) => new Date(a.localTime) - new Date(b.localTime));
 });
